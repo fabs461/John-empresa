18ed56c962e2d10c6f4fcac9f37b1fe348ca1e52
fabs461 <fabricio.perez.salaues@gmail.com>
2026-08-03 19:40:28 -0400

    Database add promise use

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
