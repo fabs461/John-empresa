ff123501d57f234c4bccabe5240755a511c53e5b
fabs461 <fabricio.perez.salaues@gmail.com>
2026-08-07 20:23:44 -0400

    deleted redundancy from orderRoutes

--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -6,14 +6,6 @@ const verifyToken = require('../middleware/authMiddleware');
 // Ruta pública: el cliente envía su pedido desde el carrito
 router.post('/', orderController.createOrder);
 
-const express = require('express');
-const router = express.Router();
-const orderController = require('../controllers/orderController');
-const verifyToken = require('../middleware/authMiddleware');
-
-// Ruta pública: el cliente envía su pedido desde el carrito
-router.post('/', orderController.createOrder);
-
 // Ruta protegida: solo el administrador ve la lista de pedidos
 router.get('/', verifyToken, orderController.getAllOrders);
 
